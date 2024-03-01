59046425c1ea6580e3baf671d1af550b19a9446c
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-01 10:35:00 -0300

    lista 1 - entrada saída operadores logico e relacionais

---
+++ Listas/Lista1/exercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite sua idade em dias");
+        int idadeEmDias = int.Parse(Console.ReadLine());
+
+        int anos = idadeEmDias / 365;
+        bool maiorDeIdade = anos >= 18;
+
+        Console.WriteLine("Essa pessoa é maior de idade? " + maiorDeIdade);
+        
+    }
+}
