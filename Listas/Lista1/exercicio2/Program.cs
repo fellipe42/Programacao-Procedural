81b14249a34bbea7f4843960aecf7d759f600faf
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-01 22:53:38 -0300

    lista 1 exercicio 2 - calculo da base de triangulo e teste logico

---
+++ Listas/Lista1/exercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira a base do triangulo");
+        Double ValorBase = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira a altura do triangulo");
+        double ValorAltura = double.Parse(Console.ReadLine());
+
+        double Area = ((ValorBase * ValorAltura)/ 2);
+
+        bool Resultado = Area > 20;
+        Console.WriteLine("Qual a area do triangulo? " + Area);
+        Console.WriteLine("A area do triangulo eh maior que 20? " + Resultado);
+        
+    }
+}
