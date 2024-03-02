2948cd132daaca0030623b92a5ef8203362ee429
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-01 23:17:30 -0300

    lista 1 exercicio 3 contagem de centavos

---
+++ Listas/Lista1/exercicio3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Insira a quantidade de centavos que deseja converter");
+      double TotalCentavos = double.Parse(Console.ReadLine());
+
+      double Reais = (TotalCentavos / 100);
+      double Centavos = (TotalCentavos % 100);
+
+    Console.WriteLine("O total em dinheiro eh: " + Reais + " real(is) e " + Centavos + " centavo(s).");
+
+    }
+}
