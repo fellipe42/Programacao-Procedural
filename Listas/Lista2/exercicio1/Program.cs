b585622602f45c60f925c8ba8e8a737a62db8c65
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-01 23:41:20 -0300

    Lista 2 exercicio 1 - login e senha, primeiro programa com condicional

---
+++ Listas/Lista2/exercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Insira o nome de usuario");
+      String Usuario = (Console.ReadLine());
+
+      Console.WriteLine("Insira a senha");
+      String Senha = (Console.ReadLine());
+
+      bool Resultado = (Usuario == "admin") && (Senha == "123senha");
+
+      if (Resultado == true)
+      {
+        Console.WriteLine("Login realizado com sucesso!");
+      }
+      else
+      {
+        Console.WriteLine("Credenciais inválidas…");
+      }
+
+    }
+}
