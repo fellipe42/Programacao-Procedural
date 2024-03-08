5166b926c637a58f44836e93062b05dcd5250e89
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-08 18:38:01 -0300

    lista 3 exercicio 2 - exercicio com while e condicional

---
+++ Listas/Lista3/exercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string [] args)
+    {
+        int valorGasto = 0;
+        int quantidadeDeLidas = 0;
+
+            Console.WriteLine("Digite a quantidade de itens comprados \n");
+        int quantidadeDeItens = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o preco de cada um dos itens \n");
+
+        while (quantidadeDeLidas < quantidadeDeItens)
+        {
+            quantidadeDeLidas ++;
+            int ValorDoItem = int.Parse(Console.ReadLine());
+            valorGasto = (valorGasto + ValorDoItem);
+        }
+
+        if (valorGasto >= 150)
+        {
+            Console.WriteLine("Parabens! Voce ganhou frete gratis em sua compra de valor R$" + valorGasto);
+        }
+        else 
+        {
+            Console.WriteLine("A compra nao possui frete gratis");
+        }
+    }
+}
