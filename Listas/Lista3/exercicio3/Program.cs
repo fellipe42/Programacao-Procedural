53f07db83bd853fe8ce96b698512b4f323bdb2d8
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-15 23:20:29 -0300

    exercicio 3 lista 3

---
+++ Listas/Lista3/exercicio3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        const string NOME_DE_USUARIO = "admin";
+        const string SENHA = "123senha";
+        const int MAXIMO_TENTATIVAS = 3;
+
+
+        int contadorTentativas = 1;
+
+        do
+        {
+            Console.WriteLine("\nDigite seu nome de usuário");
+            string UsuarioDigitado = Console.ReadLine();
+
+            Console.WriteLine("\nDigite sua senha");
+            string SenhaDigitada = Console.ReadLine();
+
+            contadorTentativas ++;
+
+            if ((UsuarioDigitado == NOME_DE_USUARIO) && (SenhaDigitada == SENHA))
+            {
+                Console.WriteLine("Login realizado com sucesso");
+                break;
+            }
+        }
+        while (contadorTentativas <= MAXIMO_TENTATIVAS);
+
+            if (contadorTentativas > MAXIMO_TENTATIVAS)
+            {
+                Console.WriteLine("Sua conta foi bloqueada");
+            }
+
+    }
+}
