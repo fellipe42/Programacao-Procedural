232ca366751d207885736bca53da0434d2effd76
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-15 23:53:38 -0300

    lista 4 exercicio 1 condicionais aninhadas

---
+++ Listas/Lista4/exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        int contadorMaior = 1;
+
+        while (contadorMaior <10)
+        {
+            for (int contador = 1; contador <= 10; contador ++)
+            {
+                Console.WriteLine(contadorMaior + " x " + contador + " = " + (contadorMaior * contador));
+            }
+            contadorMaior ++;
+            Console.WriteLine("\n");
+        }
+        
+    }
+}
