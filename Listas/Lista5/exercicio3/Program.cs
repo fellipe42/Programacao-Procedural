cf35f743a3b0e5eef430d902de8db2b14e0ad966
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-25 22:44:12 -0300

    exercicio 3, lista 5 - lacos de repeticao aninhados , retangulos de asteriscos

---
+++ Listas/Lista5/exercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira o tamanho da altura do retangulo: ");
+        int alturaDoRetangulo = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira o tamanho da base do retangulo");
+        int baseDoRetangulo = int.Parse(Console.ReadLine());
+
+        for (int contadorLinha = 0; contadorLinha < alturaDoRetangulo; contadorLinha++)
+        {
+            for (int contadorColuna = 0; contadorColuna < baseDoRetangulo; contadorColuna++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
