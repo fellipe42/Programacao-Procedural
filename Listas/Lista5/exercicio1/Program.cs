9d5c26d5239617664c599a26654088a3fa436524
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-25 22:10:48 -0300

    exercicio 1, lista 5 - laco de repeticao com dois contadores internos

---
+++ Listas/Lista5/exercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int LIMITE_CRESCENTE = 100;
+        const int LIMITE_DECRESCENTE = 0;
+        int contador = 0;
+        int decontador = 200;
+
+        for (contador = 0, decontador = 200; contador <= LIMITE_CRESCENTE && decontador >= LIMITE_DECRESCENTE;
+            contador += 2, decontador -= 4)
+        {
+            Console.WriteLine(contador + "-" + decontador);
+        }
+    }
+}
