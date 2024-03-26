cba59828cf542322001478ab29980f8bc4c8728f
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-25 22:23:35 -0300

    exercicio 2, lista 5 - laco de repeticao aninhado e concatenacao com cifrao

---
+++ Listas/Lista5/exercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int primeiroFator = 10;
+
+        while (primeiroFator > 0)
+        {
+            for (int segundoFator = 1; segundoFator <= 10; segundoFator++)
+            {
+                Console.WriteLine($"{primeiroFator} x {segundoFator} = {primeiroFator * segundoFator}");
+            }
+            primeiroFator--;
+            Console.WriteLine("\n");
+        }
+    }
+}
