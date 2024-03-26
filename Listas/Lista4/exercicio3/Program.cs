be0b791390f7ff6f248f21d4b60fbf5fa6ff778e
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-25 21:43:12 -0300

    exercicio 3, lista 4 - aplicando laco de repeticao em matriz de tabuleiro

---
+++ Listas/Lista4/exercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml;
+using Microsoft.VisualBasic;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Tabuleiro.InicializaTabuleiro();
+
+        for (int coluna = 0; coluna < 8; coluna++)
+        {
+            for (int linha = 0; linha < 8; linha++)
+            {
+                if (coluna == 0 || coluna == 7 || linha == 0 || linha == 7)
+                {
+                    Tabuleiro.Adicionar(linha, coluna, "S");
+                }
+            }
+        }
+        
+        Tabuleiro.ImprimeTabuleiro();
+    }
+}
