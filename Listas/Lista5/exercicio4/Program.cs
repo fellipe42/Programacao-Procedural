74595ceacbe3b68c62d5ab52072c2c0610ada1c1
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-26 17:39:04 -0300

    exercicio 4, lista 5 - somatorio com numeros impares

---
+++ Listas/Lista5/exercicio4/Program.cs
@@ -0,0 +1,30 @@
+﻿//Crie um programa que receba números inteiros através do teclado e 
+//acumule todos os números ímpares digitados em um somatório. 
+// Quando um número negativo for digitado, seu programa deve parar de acumular os números e exibir o resultado do somatório de ímpares.
+//OBS: O número negativo não deve ser adicionado ao somatório.
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int numeroDigitado = 0;
+        int somatorio = 0;
+
+        Console.WriteLine("Insira numeros inteiros: ");
+
+        do
+        {
+            numeroDigitado = int.Parse(Console.ReadLine());
+
+            if (numeroDigitado % 2 == 1 && numeroDigitado > 0)
+            {
+                somatorio = somatorio + numeroDigitado;
+            }
+        }
+        while (numeroDigitado >= 0);
+
+        Console.WriteLine($"a soma de numeros impares foi igual a: {somatorio}");
+    }
+}
