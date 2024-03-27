ac077d493b0633e95e6a8e0564101e73997381d5
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-27 19:11:11 -0300

    lista 6, exercicio 1 - vetores com string

---
+++ Listas/Lista6/exercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] nomes = new string[10];
+        int numeroDeNomes = 10;
+        string nome;
+        int contador;
+
+        System.Console.WriteLine($"Insira {numeroDeNomes} nomes: ");
+
+        for (int i = 0; i < numeroDeNomes; i++)
+        {
+            nomes[i] = Console.ReadLine();
+        }
+
+        System.Console.WriteLine("Insira o nome que deseja buscar na lista: ");
+        nome = Console.ReadLine();
+
+        for (contador = 0; contador < numeroDeNomes; contador++)
+        {
+            if (nomes[contador] == nome)
+            {
+                Console.WriteLine("ACHEI");
+                break;
+            }
+            else if (contador == (numeroDeNomes - 1))
+            {
+                Console.WriteLine("NAO ACHEI");
+                break;
+            }
+        }
+    }
+}
