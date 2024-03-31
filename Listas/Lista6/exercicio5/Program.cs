f5fa43baaee9fc09123302178591b148c5735eb0
fellipe42 <fellipe.mvj@hotmail.com>
2024-03-31 14:55:14 -0300

    vetores com leitura e impressao seletiva

---
+++ Listas/Lista6/exercicio5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_VETOR = 10;
+        int[] numeros = new int[TAMANHO_VETOR];
+        int contador = 0;
+        int entrada = 0;
+
+        Console.WriteLine("Insira numeros inteiros: ");
+
+        do
+        {
+            entrada = int.Parse(Console.ReadLine());
+            if (entrada >= 10 && entrada <= 50)
+            {
+                numeros[contador] = entrada;
+                contador++;
+            }
+
+        } while (contador < TAMANHO_VETOR);
+
+        Console.WriteLine();
+
+        for (int colunas = 0; colunas < TAMANHO_VETOR; colunas++)
+        {
+            Console.WriteLine(numeros[colunas]);
+        }
+    }
+}
