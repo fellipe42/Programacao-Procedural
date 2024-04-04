4e1889f0735c9e32d17436dc25dc18e4e8b7c70f
fellipe42 <fellipe.mvj@hotmail.com>
2024-04-04 11:18:36 -0300

    funcao com laco de repeticao e vetor

---
+++ Listas/Lista7/exercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static int MostraMaior(int[] vetor)
+    {
+        int maiorElemento = int.MinValue;
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] > maiorElemento)
+            {
+                maiorElemento = vetor[i];
+            }
+        }
+        return maiorElemento;
+    }
+    static void Main(string[] args)
+    {
+        int[] Entrada = new int[3];
+
+        for (int i = 0; i < Entrada.Length; i++)
+        {
+           Entrada[i] = int.Parse(Console.ReadLine());
+        }
+
+        int resultado = MostraMaior(Entrada);
+        Console.WriteLine(resultado);
+    }
+}
