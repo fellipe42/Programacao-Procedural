f853f9309b298e40b3a422de6f0a4453faae0c38
fellipe42 <fellipe.mvj@hotmail.com>
2024-04-04 22:38:29 -0300

    funcao com condicionais

---
+++ Listas/Lista7/exercicio3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static string ClassificarNadador(int idade)
+    {
+        if (idade < 12)
+        {
+            return "infantil";
+        }
+        else if (idade >= 12 && idade <= 14)
+        {
+            return "Juvenil A";
+        }
+        else if (idade >= 15 && idade <= 17)
+        {
+            return "Juvenil B";
+        }
+        else if (idade >= 18)
+        {
+            return "Adulto";
+        }
+        else
+        {
+            return "Insira uma idade valida";
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira a idade do jogador: ");
+
+        int idade = int.Parse(Console.ReadLine());
+
+        string resultado = ClassificarNadador(idade);
+
+        Console.WriteLine(resultado);
+    }
+}
