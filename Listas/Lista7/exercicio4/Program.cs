655d20807f148370c2ab20572fe3632862e4fcf5
fellipe42 <fellipe.mvj@hotmail.com>
2024-04-05 00:44:37 -0300

    criando e utilizando duas funcoes

---
+++ Listas/Lista7/exercicio4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+
+    static void PreencheVetor(int[] vetor)
+    {
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+    }
+
+    static void TestePeso(int[] vetor)
+    {
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] > 50)
+            {
+                Console.WriteLine((i + 1) + "º vagao ultrapassou o limite de peso");
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        const int TAMANHO_TREM = 12;
+        int[] trem = new int[TAMANHO_TREM];
+
+        Console.WriteLine("Insira o peso de cada um dos " + TAMANHO_TREM + " vagoes");
+        PreencheVetor(trem);
+        TestePeso(trem);
+    }
+}
