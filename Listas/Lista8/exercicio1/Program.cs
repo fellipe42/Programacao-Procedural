eb2b4dcf1c8b07defe1ff3a0d0bf9bf494c4e592
fellipe42 <fellipe.mvj@hotmail.com>
2024-04-12 23:33:32 -0300

    Lista 8 exercicio 1, structures

---
+++ Listas/Lista8/exercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza1;
+
+        pizza1.id = int.Parse(Console.ReadLine());
+        pizza1.sabor = Console.ReadLine();
+        pizza1.preco = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(pizza1.id);
+        Console.WriteLine(pizza1.sabor);
+        Console.WriteLine(pizza1.preco);
+
+    }
+}
