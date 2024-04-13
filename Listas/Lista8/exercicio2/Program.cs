b6a2711c97804115d76963f00f0581a52c208556
fellipe42 <fellipe.mvj@hotmail.com>
2024-04-13 00:32:22 -0300

    lista 8, exercicio2 - structure com vetor

---
+++ Listas/Lista8/exercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_VETOR = 4;
+        Produto[] vetor = new Produto[TAMANHO_VETOR];
+
+        for (int contador = 0; contador < TAMANHO_VETOR; contador++)
+        {
+            vetor[contador].id = int.Parse(Console.ReadLine());
+            vetor[contador].nome = Console.ReadLine();
+            vetor[contador].preco = double.Parse(Console.ReadLine());
+            vetor[contador].disponivelEmEstoque = bool.Parse(Console.ReadLine());
+        }
+
+        for (int contador = 0; contador < TAMANHO_VETOR; contador++)
+        {
+            if (vetor[contador].disponivelEmEstoque == true)
+            {
+                Console.WriteLine(vetor[contador].nome);
+                Console.WriteLine(vetor[contador].preco);
+            }
+        }
+
+    }
+}
